7db1199f17302b9356a65f175ecbfd931507594f
Anna Sobova <anna_sobova@ukr.net>
2023-01-30 20:32:01 +0200

    movie name page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { lazy } from 'react';
 
 const SharedLayout= lazy(()=>import('./SharedLayout/SharedLayout'));
 const Home = lazy(()=>import('../pages/Home'));
-const Movies = lazy(()=> import('../pages/Movies'));
+const Movies = lazy(()=> import('../pages/Movies1'));
 const MovieInfo = lazy(()=> import('../pages/MovieDetails'));
 const Cast = lazy(()=> import('../components/Cast/Cast'))
 const Reviews = lazy(() => import('../components/Reviews/Reviews'));
@@ -14,7 +14,7 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<Home />} />
-          <Route path="movies" element={<Movies />} />
+          <Route path="movies" element={<Movies/>} />
           <Route path="movies/:movieId" element={<MovieInfo />}>
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
